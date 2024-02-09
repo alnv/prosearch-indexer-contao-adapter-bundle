7117a8a6330a8129fcf0a56d1ac7e940d4197e2d
Alexander Naumov <alexandernaumov@AlexandGeschaft.fritz.box>
2024-02-09 16:31:48 +0100

    allow to rm css

--- contao/languages/en/tl_module.php
+++ contao/languages/en/tl_module.php
@@ -1,6 +1,7 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_module']['search_legend'] = 'Such-Einstellungen';
+$GLOBALS['TL_LANG']['tl_module']['style_legend'] = 'Style-Einstellungen';
 
 $GLOBALS['TL_LANG']['tl_module']['psSearchCategories'] = ['Kategorien', 'Die Suche liefert nur die ausgewählten Kategorien'];
 $GLOBALS['TL_LANG']['tl_module']['psAutoCompletionType'] = ['Art der Suche', ''];
@@ -8,6 +9,7 @@ $GLOBALS['TL_LANG']['tl_module']['psResultsTemplate'] = ['Template', ''];
 $GLOBALS['TL_LANG']['tl_module']['psAnalyzer'] = ['Search analyzer', ''];
 $GLOBALS['TL_LANG']['tl_module']['psOpenDocumentInBrowser'] = ['Dokumente direkt im Browser öffnen', ''];
 $GLOBALS['TL_LANG']['tl_module']['psUseRichSnippets'] = ['RichSnippets verwenden', ''];
+$GLOBALS['TL_LANG']['tl_module']['psPreventCssLoading'] = ['Keine CSS-Dateien laden', ''];
 
 $GLOBALS['TL_LANG']['tl_module']['psReference'] = [];
 $GLOBALS['TL_LANG']['tl_module']['psReference']['simple'] = ['Wortvervollständigung', ''];
