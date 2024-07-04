a0f7aaa85e8b3295c1ec4add4ce373a5a55fc430
Alexander Naumov <alexandernaumov@AlexandGeschaft.fritz.box>
2024-07-04 14:05:50 +0200

    fx stringutil namespace

--- src/Helpers/Stats.php
+++ src/Helpers/Stats.php
@@ -80,7 +80,7 @@ class Stats
 
         while ($objStat->next()) {
 
-            $arrStatTypes = \StringUtil::deserialize($objStat->types, true);
+            $arrStatTypes = StringUtil::deserialize($objStat->types, true);
 
             if (empty(array_diff($arrTypes, $arrStatTypes))) {
 
