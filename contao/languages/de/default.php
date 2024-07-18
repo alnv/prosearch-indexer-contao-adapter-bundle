d98206a94a50b7857341c7df3d8d89979d4d146b
Alexander Naumov <alexandernaumov@AlexandGeschaft.fritz.box>
2024-07-18 18:06:14 +0200

    fx

--- contao/languages/de/default.php
+++ contao/languages/de/default.php
@@ -5,7 +5,7 @@ $GLOBALS['TL_LANG']['MSC']['searchResultsForLabel'] = 'Ergebnisse für ';
 $GLOBALS['TL_LANG']['MSC']['searchResultsCategoriesHeadline'] = 'Kategorien';
 $GLOBALS['TL_LANG']['MSC']['noSearchResults'] = 'Keine Suchergebnisse gefunden.';
 $GLOBALS['TL_LANG']['MSC']['searchResultsCloseButtonLabel'] = 'Schließen';
-$GLOBALS['TL_LANG']['MSC']['searchResultsMoreButton'] = 'Weitere Ergebnisse laden';
+$GLOBALS['TL_LANG']['MSC']['searchResultsMoreButton'] = 'Mehr laden';
 
 $GLOBALS['TL_LANG']['MSC']['searchButtonIcon'] = '/bundles/alnvprosearchindexercontaoadapter/search-icon.svg';
 $GLOBALS['TL_LANG']['MSC']['searchButtonLoadingIcon'] = '/system/themes/flexible/icons/loading.svg';
