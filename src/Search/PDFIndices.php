d59f052f01dfc512af7d11a86b1a15345a5c1bcd
Alexander Naumov <alexandernaumov@MacBookPro.fritz.box>
2026-05-07 09:48:58 +0200

    fx warning

--- src/Search/PDFIndices.php
+++ src/Search/PDFIndices.php
@@ -8,15 +8,15 @@ use Alnv\ProSearchIndexerContaoAdapterBundle\Helpers\States;
 use Alnv\ProSearchIndexerContaoAdapterBundle\Helpers\Text;
 use Alnv\ProSearchIndexerContaoAdapterBundle\Models\IndicesModel;
 use Contao\CoreBundle\Monolog\ContaoContext;
-use Contao\Frontend;
+use Contao\CoreBundle\Search\Document;
 use Contao\File;
-use Contao\StringUtil;
 use Contao\FilesModel;
-use Contao\System;
+use Contao\Frontend;
 use Contao\PageModel;
+use Contao\StringUtil;
+use Contao\System;
 use Psr\Log\LogLevel;
 use Smalot\PdfParser\Parser;
-use Contao\CoreBundle\Search\Document;
 use Symfony\Component\DomCrawler\Crawler;
 
 class PDFIndices extends Searcher
@@ -75,7 +75,7 @@ class PDFIndices extends Searcher
 
                 System::getContainer()
                     ->get('monolog.logger.contao')
-                    ->log(LogLevel::ERROR, 'PDF Parser ('.$objFile->path.'): MAX 5mb allowed!', ['contao' => new ContaoContext(__CLASS__ . '::' . __FUNCTION__)]);
+                    ->log(LogLevel::ERROR, 'PDF Parser (' . $objFile->path . '): MAX 5mb allowed!', ['contao' => new ContaoContext(__CLASS__ . '::' . __FUNCTION__)]);
 
                 continue;
             }
@@ -132,7 +132,7 @@ class PDFIndices extends Searcher
 
                 if (!in_array('preventIndexMetadata', $arrSettings)) {
                     $objIndicesModel->images = ['assets/contao/images/pdf.svg'];
-                    $objIndicesModel->title = (($strMetaTitle ?: ($strTitleAttr?:$strNodeContent)) ?: $strFilename);
+                    $objIndicesModel->title = (($strMetaTitle ?: ($strTitleAttr ?: $strNodeContent)) ?: $strFilename);
                     $objIndicesModel->description = ($strMetaDescription ?: $strMetaAlt);
                 }
 
@@ -161,7 +161,7 @@ class PDFIndices extends Searcher
             } catch (\Exception $exception) {
                 System::getContainer()
                     ->get('monolog.logger.contao')
-                    ->log(LogLevel::ERROR, 'PDF Parser ('.$objFile->path.'): ' . $exception->getMessage(), ['contao' => new ContaoContext(__CLASS__ . '::' . __FUNCTION__)]);
+                    ->log(LogLevel::ERROR, 'PDF Parser (' . $objFile->path . '): ' . $exception->getMessage(), ['contao' => new ContaoContext(__CLASS__ . '::' . __FUNCTION__)]);
             }
         }
     }
@@ -174,6 +174,6 @@ class PDFIndices extends Searcher
 
         return (isset($arrParams['f']) && \str_ends_with(strtolower($arrParams['f']), '.pdf')) ||
             (isset($arrParams['p']) && \str_ends_with(\strtolower($arrParams['p']), '.pdf')) ||
-            (\str_ends_with(\strtolower($arrUrl['path']), '.pdf'));
+            (\str_ends_with(\strtolower($arrUrl['path'] ?? ''), '.pdf'));
     }
 }
